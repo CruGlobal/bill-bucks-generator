4a3de9e2f14eb14c5dfb21a2b7b61a121a64a236
Shelby Benton <shelby.benton@cru.org>
2020-10-09 14:56:32 -0400

    added multiple text entries that auto fill the image with user input

--- app/controllers/bucks_controller.rb
+++ app/controllers/bucks_controller.rb
@@ -1,25 +1,38 @@
-require 'rmagick'
+# require 'rmagick'
 
 class BucksController < ApplicationController
-  include ::Magick
-
+  
   def new
-    @image_params = params.permit(:to, :from, :for, :type, :commit).to_h
+    @image_params = post_params
   end
 
   def img
+    @image_params = post_params
     img = ImageList.new('public/bill.png')
     txt = Draw.new
-
-    img.annotate(txt, 0,0,70,300, "Spencer Oberstadt test") do
+    
+    img.annotate(txt, 0,0,70,295, @image_params[:to]) do
       txt.gravity = Magick::NorthWestGravity
       txt.pointsize = 25
-      txt.stroke = '#000000'
-      txt.fill = '#ffffff'
-      txt.font_weight = Magick::BoldWeight
+    end
+
+    img.annotate(txt, 0,0,385,295, @image_params[:from]) do
+      txt.gravity = Magick::NorthWestGravity
+      txt.pointsize = 25
+    end
+
+    img.annotate(txt, 0,0,85,350, @image_params[:for]) do
+      txt.gravity = Magick::NorthWestGravity
+      txt.pointsize = 25
     end
 
     img.format = 'jpeg'
     send_data img.to_blob, :stream => 'false', :filename => 'test.jpg', :type => 'image/jpeg', :disposition => 'inline'
+  end
+
+  private
+
+  def post_params
+    @image_params = params.permit(:to, :from, :for, :type, :commit, :format).to_h
   end
 end
