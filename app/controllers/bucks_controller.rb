5617ae68d17b27cf20311c8abafcec338a0f9131
Spencer Oberstadt <soberstadt@gmail.com>
2020-10-16 15:05:34 -0400

    cleanup image code a little

--- app/controllers/bucks_controller.rb
+++ app/controllers/bucks_controller.rb
@@ -2,31 +2,21 @@ class BucksController < ApplicationController
   include ::Magick
 
   def new
-    @image_params = post_params
+    @image_params = image_params
   end
 
   def img
-    @image_params = post_params
-    img = bill_or_vonette(@image_params[:type])
+    img = bill_or_vonette(image_params[:type])
     txt = Draw.new
-    
-    img.annotate(txt, 0,0,70,295, @image_params[:to]) do
-      txt.gravity = Magick::NorthWestGravity
-      txt.pointsize = 25
-    end
+    txt.pointsize = 25
+    txt.gravity = Magick::NorthWestGravity
 
-    img.annotate(txt, 0,0,385,295, @image_params[:from]) do
-      txt.gravity = Magick::NorthWestGravity
-      txt.pointsize = 25
-    end
+    img.annotate(txt, 0,0,70,295, image_params[:to])
+    img.annotate(txt, 0,0,385,295, image_params[:from])
+    img.annotate(txt, 0,0,85,350, image_params[:for])
 
-    img.annotate(txt, 0,0,85,350, @image_params[:for]) do
-      txt.gravity = Magick::NorthWestGravity
-      txt.pointsize = 25
-    end
-
-    img.format = 'jpeg'
-    send_data img.to_blob, :stream => 'false', :filename => 'test.jpg', :type => 'image/jpeg', :disposition => 'inline'
+    img.format = 'png'
+    send_data img.to_blob, stream: 'false', filename: 'test.jpg', type: 'image/jpeg', disposition: 'inline'
   end
 
   def bill_or_vonette(buck_type)
@@ -39,7 +29,7 @@ class BucksController < ApplicationController
 
   private
 
-  def post_params
-    @image_params = params.permit(:to, :from, :for, :type, :commit, :format).to_h
+  def image_params
+    @image_params ||= params.permit(:to, :from, :for, :type, :commit, :format).to_h
   end
 end
