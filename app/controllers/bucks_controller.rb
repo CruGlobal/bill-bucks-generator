43a93140696323c2a136a2d34440aa4ec3ee0d84
Spencer Oberstadt <soberstadt@gmail.com>
2020-10-16 16:13:47 -0400

    wrap text

--- app/controllers/bucks_controller.rb
+++ app/controllers/bucks_controller.rb
@@ -1,30 +1,11 @@
 class BucksController < ApplicationController
-  include ::Magick
-
   def new
-    @image_params = image_params
+    image_params
   end
 
   def img
-    img = bill_or_vonette(image_params[:type])
-    txt = Draw.new
-    txt.pointsize = 25
-    txt.gravity = Magick::NorthWestGravity
-
-    img.annotate(txt, 0,0,70,295, image_params[:to])
-    img.annotate(txt, 0,0,385,295, image_params[:from])
-    img.annotate(txt, 0,0,85,350, image_params[:for])
-
-    img.format = 'png'
-    send_data img.to_blob, stream: 'false', filename: 'test.jpg', type: 'image/jpeg', disposition: 'inline'
-  end
-
-  def bill_or_vonette(buck_type)
-    if buck_type == "vonette"
-      ImageList.new('public/vonette.png')
-    else
-      ImageList.new('public/bill.png')
-    end
+    buck = Buck.new(image_params: image_params)
+    send_data buck.to_blob, filename: "test.png", type: "image/png", disposition: "inline"
   end
 
   private
---
+++ app/models/buck.rb
@@ -0,0 +1,82 @@
+class Buck
+  attr_reader :image_params
+
+  TO_POINT_SIZE = 22
+  FOR_POINT_SIZE = 25
+
+  def initialize(image_params:)
+    @image_params = image_params
+  end
+
+  def to_blob
+    build_image.to_blob
+  end
+
+  private
+
+  def build_image
+    img = bill_or_vonette(image_params[:type])
+
+    to_text = text_instance
+    to_text.pointsize = TO_POINT_SIZE
+    img.annotate(to_text, 0, 0, 70, 300, image_params[:to])
+    img.annotate(to_text, 0, 0, 385, 300, image_params[:from])
+
+    wrapped_for_text = fit_text(image_params[:for].presence || "Unspecified", 510)
+    img.annotate(text_instance, 0, 0, 85, 350, wrapped_for_text)
+
+    img.format = "png"
+    img
+  end
+
+  def bill_or_vonette(buck_type)
+    filename = buck_type == "vonette" ? "vonette" : "bill"
+    Magick::ImageList.new("public/#{filename}.png")
+  end
+
+  def text_instance
+    txt = Magick::Draw.new
+    txt.pointsize = FOR_POINT_SIZE
+    txt.gravity = Magick::NorthWestGravity
+    txt
+  end
+
+  def text_fit?(text, width)
+    tmp_image = Magick::Image.new(width, 500)
+    drawing = text_instance
+    drawing.annotate(tmp_image, 0, 0, 0, 0, text)
+    metrics = drawing.get_multiline_type_metrics(tmp_image, text)
+
+    metrics.width < width
+  end
+
+  def fit_text(text, width)
+    separator = " "
+    line = ""
+
+    if !text_fit?(text, width) && text.include?(separator)
+      i = 0
+      text.split(separator).each do |word|
+        tmp_line = if i == 0
+          line + word
+        else
+          line + separator + word
+        end
+
+        if text_fit?(tmp_line, width)
+          unless i == 0
+            line += separator
+          end
+        else
+          unless i == 0
+            line += '\n'
+          end
+        end
+        line += word
+        i += 1
+      end
+      text = line
+    end
+    text
+  end
+end
