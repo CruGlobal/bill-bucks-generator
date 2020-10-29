9f3eace637570795b752637be458dbb7e63ded2e
Spencer Oberstadt <soberstadt@gmail.com>
2020-10-28 22:18:52 -0400

    add footer and image name

--- app/controllers/bucks_controller.rb
+++ app/controllers/bucks_controller.rb
@@ -5,10 +5,17 @@ class BucksController < ApplicationController
 
   def img
     buck = Buck.new(image_params: image_params)
-    send_data buck.to_blob, filename: "test.png", type: "image/png", disposition: "inline"
+    send_data buck.to_blob,
+              filename: "#{number}_#{image_params[:to]}_from_#{image_params[:from]}.png",
+              type: "image/png",
+              disposition: "inline"
   end
 
   private
+
+  def number
+    image_params[:type] == 'vonette' ? 5 : 1
+  end
 
   def image_params
     @image_params ||= params.permit(:to, :from, :for, :type, :commit, :format).to_h
