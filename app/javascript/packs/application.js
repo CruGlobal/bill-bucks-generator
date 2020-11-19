5a720b32700351ed305401c1930b4288a652ae30
Shelby Benton <shelby.benton@cru.org>
2020-11-19 09:54:10 -0500

    init commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,11 @@
 // that code so it'll be compiled.
 import 'bootstrap'
 require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
-
+console.log("help me")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
