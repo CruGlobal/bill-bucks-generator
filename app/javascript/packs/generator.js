0e6e7870efd094b0150462cbe1bb55810d9de0b5
Spencer Oberstadt <soberstadt@gmail.com>
2020-11-20 15:21:16 -0500

    try un-prettifying to see what the action does

--- app/javascript/packs/generator.js
+++ app/javascript/packs/generator.js
@@ -1,20 +1,18 @@
 $(() => {
-  preload();
-  console.log("dom ready");
+  preload()
+  console.log("dom ready")
 
-  $(".heart-btn").on("click", () => {
-    $("body").addClass("love-font");
+  $('.heart-btn').on('click', () => {
+    $('body').addClass('love-font');
   });
 
-  $(".radio").on("click", () => {
-    var bill_value = $(".radio:checked").val();
-    document.getElementById("img").src = eval(bill_value + ".src");
-  });
+  $('.radio').on('click', () => {
+    var bill_value = $('.radio:checked').val()
+   document.getElementById('img').src = eval(bill_value + '.src')
+  })
 });
 
 function preload() {
-  bill = new Image();
-  bill.src = "/bill.png";
-  vonette = new Image();
-  vonette.src = "/vonette.png";
+  bill = new Image; bill.src = '/bill.png'
+  vonette = new Image; vonette.src = '/vonette.png'
 }
