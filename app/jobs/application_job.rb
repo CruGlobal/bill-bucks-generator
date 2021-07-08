528071841ffe5c5493212c3b27f949ca7aca87b9
Spencer Oberstadt <soberstadt@gmail.com>
2021-07-08 14:00:45 -0400

    update prettier and prettier-ruby (#25)
    * update prettier and prettier-ruby
    
    * add rbprettier helper

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,5 @@
 # typed: strict
-class ApplicationJob < ActiveJob::Base; end # discard_on ActiveJob::DeserializationError # Most jobs are safe to ignore if the underlying records are no longer available # retry_on ActiveRecord::Deadlocked # Automatically retry jobs that encountered a deadlock
+class ApplicationJob < ActiveJob::Base
+  # discard_on ActiveJob::DeserializationError # Most jobs are safe to ignore if the underlying records are no longer available
+  # retry_on ActiveRecord::Deadlocked # Automatically retry jobs that encountered a deadlock
+end
