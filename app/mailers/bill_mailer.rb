e200ad1893321ec87f6f3c6173d68b989f2f420f
Spencer Oberstadt <soberstadt@gmail.com>
2021-07-09 17:06:58 -0400

    re-prettier

--- app/mailers/bill_mailer.rb
+++ app/mailers/bill_mailer.rb
@@ -1,7 +1,10 @@
 class BillMailer < ApplicationMailer
   def bill(to_name:, to_email:, from:, for_message:, count:)
     image_params = {
-      to: to_name, from: from, for_message: for_message, buck_type: :bill
+      to: to_name,
+      from: from,
+      for_message: for_message,
+      buck_type: :bill
     }
     buck = Buck.new(image_params)
     attachments.inline['image.png'] = buck.to_blob
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: 'localhost:3000', protocol: 'http'
+    host: 'localhost:3000',
+    protocol: 'http'
   }
 
   # Print deprecation notices to the Rails logger.
