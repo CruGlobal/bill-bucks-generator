b2f291846ffa60a75732f51a2873fe20189a19df
Spencer Oberstadt <soberstadt@gmail.com>
2021-07-16 16:35:34 -0500

    add pagination to buck message (1/x)

--- app/models/buck_wad.rb
+++ app/models/buck_wad.rb
@@ -1,21 +1,33 @@
-# typed: true
+# typed: strict
 class BuckWad
   extend T::Sig
 
+  sig { returns(T::Array[Buck]) }
   attr_reader :bucks
 
+  sig do
+    params(
+      to: T.nilable(String),
+      from: T.nilable(String),
+      for_message: T.nilable(String),
+      count: T.nilable(Integer),
+      dept: T.nilable(String)
+    ).void
+  end
   def initialize(to: '', from: '', for_message: '', count: 1, dept: '')
     image_params = { to: to, from: from, for_message: for_message, dept: dept }
     count = count.to_i
     number_of_vonetts = count / 5
     number_of_bills = count % 5
 
-    vonette = Buck.new(image_params.merge(buck_type: :vonette))
-    bill = Buck.new(image_params.merge(buck_type: :bill))
-
-    @bucks = []
-    number_of_vonetts.times { @bucks << vonette }
-    number_of_bills.times { @bucks << bill }
+    @bucks = T.let([], T::Array[Buck])
+    number_of_vonetts.times do
+      @bucks << Buck.new(image_params.merge(buck_type: :vonette))
+    end
+    number_of_bills.times do
+      @bucks << Buck.new(image_params.merge(buck_type: :bill))
+    end
+    add_pagination
   end
 
   sig { returns(T::Boolean) }
@@ -28,5 +40,16 @@ class BuckWad
     hash = {}
     @bucks.each_with_index { |buck, i| hash[buck.filename(i)] = buck }
     hash
+  end
+
+  private
+
+  sig { void }
+  def add_pagination
+    return unless @bucks.many?
+
+    @bucks.each_with_index do |buck, i|
+      buck.for_message = "#{buck.for_message} (#{i + 1}/#{@bucks.count})"
+    end
   end
 end
---
+++ spec/models/buck_wad_spec.rb
@@ -0,0 +1,24 @@
+# typed: false
+require 'rails_helper'
+
+RSpec.describe BuckWad, type: :model do
+  describe '#bucks' do
+    subject { wad.bucks }
+    let(:for_message) { 'test message' }
+    context 'with multiple bucks' do
+      let(:wad) { BuckWad.new(for_message: for_message, count: 7) }
+      it 'returns multiple bucks with count indicator' do
+        expect(subject.count).to be 3
+        expect(subject.map(&:for_message).uniq.count).to be 3
+      end
+    end
+
+    context 'with one buck' do
+      let(:wad) { BuckWad.new(for_message: for_message, count: 5) }
+      it 'returns one bucks without count indicator' do
+        expect(subject.count).to be 1
+        expect(subject.first.for_message).to eq for_message
+      end
+    end
+  end
+end
