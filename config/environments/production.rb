172eda930e0695c58dab1265ecf2c9369114b9cf
Spencer Oberstadt <soberstadt@gmail.com>
2021-10-06 11:50:38 -0500

    fix mailer url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'bill-buck-generator.herokuapp.com/'
+  host = 'billbuck.app'
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
