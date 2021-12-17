5d15d4685bdb346bb938764aecdc0f231cb660c7
dbenton9 <dbenton9@users.noreply.github.com>
2021-12-17 16:53:44 +0000

    Prettified Code!

--- babel.config.js
+++ babel.config.js
@@ -54,10 +54,10 @@ module.exports = function (api) {
         },
       ],
       [
-        '@babel/plugin-proposal-private-methods',
+        "@babel/plugin-proposal-private-methods",
         {
-          loose: true
-        }
+          loose: true,
+        },
       ],
       [
         "@babel/plugin-transform-runtime",
