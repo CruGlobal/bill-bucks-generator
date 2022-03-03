4f6cf8451c8f87b7483c21aae7892d821c6fff1c
Brian Zoetewey <brian.zoetewey@cru.org>
2022-03-03 16:52:47 -0500

    Update build for GitHub Actions.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build \
+docker buildx build $DOCKER_ARGS \
     --build-arg DD_API_KEY=$DD_API_KEY \
     --build-arg SECRET_KEY_BASE=$SECRET_KEY_BASE \
-    -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$ENVIRONMENT-$BUILD_NUMBER .
+    .
 rc=$?
 
 if [ $rc -ne 0 ]; then
