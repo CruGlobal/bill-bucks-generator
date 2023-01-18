bbe118787443ec1100c477ddef486badaaa05134
Shelby Benton <shelby.benton@cru.org>
2023-01-18 11:11:55 -0500

    prettier

--- app/lib/email_validator.rb
+++ app/lib/email_validator.rb
@@ -1,6 +1,11 @@
 module EmailValidator
   def self.cru?(email)
-    valid_suffixes = %w[@cru.org @jesusfilm.org @cru-main.slack.com @athletesinaction.org]
+    valid_suffixes = %w[
+      @cru.org
+      @jesusfilm.org
+      @cru-main.slack.com
+      @athletesinaction.org
+    ]
     valid_suffixes.any? { |suffix| email.ends_with?(suffix) }
   end
 end
