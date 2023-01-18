94e5410a624bb151f225f5d4be0c3b8aaf52008d
Shelby Benton <shelby.benton@cru.org>
2023-01-18 10:57:23 -0500

    expanding allowable email recipients

--- app/lib/email_validator.rb
+++ app/lib/email_validator.rb
@@ -1,6 +1,6 @@
 module EmailValidator
   def self.cru?(email)
-    valid_suffixes = %w[@cru.org @jesusfilm.org @cru-main.slack.com]
+    valid_suffixes = %w[@cru.org @jesusfilm.org @cru-main.slack.com @athletesinaction.org]
     valid_suffixes.any? { |suffix| email.ends_with?(suffix) }
   end
 end
